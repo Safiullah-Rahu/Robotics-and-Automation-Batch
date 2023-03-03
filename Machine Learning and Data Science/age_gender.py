55605fa8ad3026c3767127bee4054b94632defa1
Safiullah Rahu <safiullahrahu1555@gmail.com>
2023-03-03 13:39:55 +0500

    Add files via upload

---
+++ Machine Learning and Data Science/age_gender.py
@@ -0,0 +1,62 @@
+import face_recognition
+import cv2
+
+# Loading video for face detection
+video_capture = cv2.VideoCapture(0)
+
+faceProto="opencv_face_detector.pbtxt"
+faceModel="opencv_face_detector_uint8.pb"
+ageProto="age_deploy.prototxt"
+ageModel="age_net.caffemodel"
+genderProto="gender_deploy.prototxt"
+genderModel="gender_net.caffemodel"
+
+MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList=['Male','Female']
+
+ageNet=cv2.dnn.readNet(ageModel,ageProto)
+genderNet=cv2.dnn.readNet(genderModel,genderProto)
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_frame = frame[:, :, ::-1]
+
+    # Find all the faces and face enqcodings in the frame of video
+    face_locations = face_recognition.face_locations(rgb_frame)
+
+    # Loop through each face in this frame of video
+    for top, right, bottom, left in face_locations:
+        face_image = frame[top:bottom, left:right]
+        blob=cv2.dnn.blobFromImage(face_image, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds=genderNet.forward()
+        gender=genderList[genderPreds[0].argmax()]
+        print(f'Gender: {gender}')
+
+        ageNet.setInput(blob)
+        agePreds=ageNet.forward()
+        age=ageList[agePreds[0].argmax()]
+        print(f'Age: {age[1:-1]} years')
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right + 50, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, f'{gender}, {age}', (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
